149b494d9ebd500e6830d2fc2f9b9f56d11f85ca
ilimKUSH <ilimKUSH>
2022-07-20 15:01:29 +0600

    crud2

--- src/components/ProductsCard/ProductsCard.jsx
+++ src/components/ProductsCard/ProductsCard.jsx
@@ -1,4 +1,3 @@
-import { CardMedia } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { productsContext } from "../../contexts/productsContext";
@@ -6,14 +5,17 @@ import { productsContext } from "../../contexts/productsContext";
 export default function ProductsCard({ item }) {
   const navigate = useNavigate();
   const { deleteProduct } = React.useContext(productsContext);
-  console.log(item);
+  {
+    console.log(item);
+  }
+
   return (
     <div className="products">
       <div className="container">
         <div className="products__content">
           <img src={item.image} width={100} alt="qweqwe" />
           <h3>{item.name}</h3>
-          <h3>{item.price}</h3>
+          <h3>{item.price} сом</h3>
           <button onClick={() => deleteProduct(item.id)}>delete</button>
           <button onClick={() => navigate(`/edit/${item.id}/`)}>edit</button>
           <button onClick={() => navigate(`/products/${item.id}`)}>
